a03aeab86925e80fbd0aa353f382894fbcd2b194
Robert Stuttaford <robert@cognician.com>
2015-03-19 10:08:31 +0200

    Add more data after a timeout.

--- src/app.cljs
+++ src/app.cljs
@@ -1,13 +1,19 @@
 (ns app
   (:require cljsjs.c3))
 
+(enable-console-print!)
+
 (def chart-data
   {:bindto "#chart"
    :data   {:columns [["data1" 30 200 100 400 150 250]
                       ["data2" 50 20 10 40 15 25]]}})
 
-(enable-console-print!)
+(def extra-chart-data
+  {:columns [["data3" 400 500 450 700 600 500]]})
+
+(defonce chart (atom nil))
 
 (defn init []
   (prn chart-data)
-  (js/c3.generate (clj->js chart-data)))
+  (reset! chart (js/c3.generate (clj->js chart-data)))
+  (js/setTimeout (fn [] (.load @chart (clj->js extra-chart-data)))))
